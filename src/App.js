a8ef4ea0b9928a05acccfa133750561a4a63ea90
ChaseKnowlden <knowlden887@gmail.com>
2020-02-13 17:14:00 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState,useEffect } from 'react';
+import Header from './Components/Header'
+import GridData from './Components/GridData'
+import { CssBaseline } from '@material-ui/core';
+import axios from "axios"
+
 
 function App() {
+
+  const [ForeCast,SetForeCast]=useState(null);
+  const [GeneralData,SetGeneralData]=useState(null);
+
+  /* useEffect(()=>{
+    axios.get(process.env.REACT_APP_FORCAST_BASE_URL.replace("{MYCITY}","mashhad")).then((res)=>{
+      setTimeout(()=>{SetForeCast(res)},3000)
+    })
+  },[ForeCast])
+  useEffect(()=>{
+    axios.get(process.env.REACT_APP_CURRENT_BASE_URL.replace("{MYCITY}","mashhad")).then((res)=>{
+      setTimeout(()=>{SetGeneralData(res)},3500)
+    })
+  },[GeneralData]) */
+
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      (async()=>{
+        SetGeneralData(await axios.get(process.env.REACT_APP_CURRENT_BASE_URL.replace("{MYCITY}","mashhad")))
+        SetForeCast(await axios.get(process.env.REACT_APP_FORCAST_BASE_URL.replace("{MYCITY}","mashhad")))
+      })()
+    },1000)
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <CssBaseline />
+      <Header/>
+      <GridData component="main" GeneralData={GeneralData}  ForeCast={ForeCast} />
+      {/* <p>{process.env.REACT_APP_API_KEY}</p> */}
+    </Fragment>
   );
 }
 
